f306107108924101defccf75029cac5fbc496e9c
KevinSmithMP <kevinsmithmp4@gmail.com>
2024-12-09 16:59:03 -0300

    avance de tarea 4
    avance de la tarea 4 no se ponerlo en la base datos que falta pero ese es mi avance

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express(); 
+app.use(express.json());
+
+const cors = require('cors');
+app.use(cors());
+
+require('./db/connection');
+const Forms = require('./Models/Form')
+
+app.post("/", async(req,res)=> {
+    let Form = new Forms(req.body);
+    let result = await form.save();
+    res.send(result);
+    
+})
+
+app.listen(4000);
